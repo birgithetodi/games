0568fdf9b496d075d69880fb90ac3118d81077e1
Birgithe T. <Birgithe.todi@tptlive.ee>
2024-10-31 15:39:01 +0200

    games

---
+++ script.js
@@ -0,0 +1,9 @@
+const quizData = ["banana", "pear"]
+
+const hangman = {
+    fruits: ["banana", "watermelon", "pineapple", "blueberry", "rasberry", "strawberry"],
+    sports: ["volleyball", "football", "tennis", "basketball", "soccer", "baseball"],
+    phones: ["Iphone", "Samsung", "Nokia", "Xiomi", "Android", "Huawei"]
+}
+
+const el = document.querySelector(".hangman")
