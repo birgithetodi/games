1da094eeab5a1765a8663814435495d15b5a95d5
Birgithe T. <Birgithe.todi@tptlive.ee>
2024-11-21 15:54:32 +0200

    games

--- script.js
+++ script.js
@@ -6,12 +6,40 @@ const hangman = {
     phones: ["Iphone", "Samsung", "Nokia", "Xiomi", "Android", "Huawei"]
 }
 
+
+
 const el = document.querySelector(".hangman")
 
 function onButtonClick() {
     let element = document.querySelector('#choose-category');
     element.classList.remove('hidden');
+
+    const selectEl = document.querySelector('select')
+    selectEl.addEventListener('click', e => {
+        if (selectEl.value) {
+           console.log(selectEl.value);
+           hangman[selectEl.value]
+           
+           var arr =  ["banana", "watermelon", "pineapple", "blueberry", "rasberry", "strawberry"];
+           let optionArray = options[optionValue];
+           const randNr = Math.floor(Math.random() * optionArray.length)
+           chosenWord = optionArray[randNr];
+           chosenWord = chosenWord.toUpperCase();         
+        }
+     
+
+    });
 }
 const button = document.querySelector('#new-game-button');
 button.addEventListener('click', onButtonClick);
 
+// queryselector <select>
+// addeventlistere click
+// hangman[select.value]
+// randomiga võta listist asi
+
+let optionArray = options[optionValue];
+  //choose random word
+  const randNr = Math.floor(Math.random() * optionArray.length)
+  chosenWord = optionArray[randNr];
+  chosenWord = chosenWord.toUpperCase();
